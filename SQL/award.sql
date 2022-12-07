6794cc06e7124ab8a6ca6afce8fff9ac14d6b6cb
kbalsom <kara.balsom@keyin.com>
2022-12-07 15:23:15 -0330

    Added PG files

---
+++ SQL/award.sql
@@ -0,0 +1,16 @@
+--PETER'S CODE
+
+DROP TABLE IF EXISTS public.award;
+
+CREATE TABLE IF NOT EXISTS public.award
+(
+    award_id serial NOT NULL,
+    name character varying(60) COLLATE pg_catalog."default" NOT NULL,
+    last_update timestamp without time zone NOT NULL DEFAULT now(),
+    CONSTRAINT award_pkey PRIMARY KEY (award_id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.award
+    OWNER to postgres;
