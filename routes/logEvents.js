6f0c2fb08ba280a1cb7f605581d258b85060a3a7
kbalsom <kara.balsom@keyin.com>
2022-12-09 13:36:35 -0330

    Editing Logging Events Co-authored by David Turner

--- routes/logEvents.js
+++ routes/logEvents.js
@@ -1,7 +1,8 @@
 // npm install of date library that will be logged to dot log files, date-fns considers performace more than moment
-const { format } = require("date-fns");
+
 // npm install of UUID(Universally Unique Identifiers) that will be logged to dot log files
 const { v4: uuid } = require("uuid");
+const moment = require("moment");
 
 // This will bring in the "fs" or file structure global object no npm install required
 const fs = require("fs");
@@ -13,12 +14,19 @@ const fsPromises = require("fs").promises;
 // Path module provides a way of working with file paths
 const path = require("path");
 
+const date = String(moment().format("ll")); //This assigns the constant date to the stringified version of the current date, obtained through moment.js, in the format MMM D, YYYY.
+
+const time = String(moment().format("LTS")); //This assigns the constant time to the stringified version of the current time, obtained through moment.js, in the format H:MM:SS PM/AM:
+
+const month = moment().format("MMM"); //This assigns the constant month to the stringified version of the current month, obtained through moment.js, in the format MMM.
+
+const year = moment().year();
+
 // log events is promising to take the parameters from the emitter
 // listener in routes and send that data to the try / accept block
 // fileLineItem is the information that is written to the log file
 const logEvents = async (msg, theStatusCode) => {
-  const dateTime = `${format(new Date(), "MMM-dd-yyyy \tHH:mm:ss")}`;
-  const fileLineItem = `${dateTime} \t${msg} \t${theStatusCode} \t${uuid()}`;
+  const fileLineItem = `${date} \t${time} \t${msg} \t${theStatusCode} \t${uuid()}`;
 
   // try catch block is used to evaluate if 1 there is a filefolder to place the line item, if is does exit
   // it will take the fileLineItem and append it to the awaiting file.  If it does not exit then the folder is created
@@ -27,23 +35,29 @@ const logEvents = async (msg, theStatusCode) => {
 
   try {
     if (theStatusCode === 503) {
-      if (!fs.existsSync(path.join(__dirname, "logs"))) {
-        await fsPromises.mkdir(path.join(__dirname, "logs"));
+      if (
+        !fs.existsSync(path.join(__dirname, `${month} ${year}_status_logs`))
+      ) {
+        await fsPromises.mkdir(
+          path.join(__dirname, `${month} ${year}_status_logs`)
+        );
       }
-      const fileName =
-        `${format(new Date(), "yyyyMMdd")}` + "_503_Error_Events.log";
+      const fileName = `${date}` + "_503_Error_Events.log";
       await fsPromises.appendFile(
-        path.join(__dirname, "logs", fileName),
+        path.join(__dirname, `${month} ${year}_status_logs`, fileName),
         fileLineItem + "\n"
       );
     } else if (theStatusCode === 404) {
-      if (!fs.existsSync(path.join(__dirname, "logs"))) {
-        await fsPromises.mkdir(path.join(__dirname, "logs"));
+      if (
+        !fs.existsSync(path.join(__dirname, `${month} ${year}_status_logs`))
+      ) {
+        await fsPromises.mkdir(
+          path.join(__dirname, `${month} ${year}_status_logs`)
+        );
       }
-      const fileName =
-        `${format(new Date(), "yyyyMMdd")}` + "_404_Error_Events.log";
+      const fileName = `${date}` + "_404_Error_Events.log";
       await fsPromises.appendFile(
-        path.join(__dirname, "logs", fileName),
+        path.join(__dirname, `${month} ${year}_status_logs`, fileName),
         fileLineItem + "\n"
       );
     }
--- routes/queries.js
+++ routes/queries.js
@@ -1,7 +1,8 @@
 // npm install of date library that will be logged to dot log files, date-fns considers performace more than moment
-const { format } = require("date-fns");
+
 // npm install of UUID(Universally Unique Identifiers) that will be logged to dot log files
 const { v4: uuid } = require("uuid");
+const moment = require("moment");
 
 // This will bring in the "fs" or file structure global object no npm install required
 const fs = require("fs");
@@ -13,12 +14,19 @@ const fsPromises = require("fs").promises;
 // Path module provides a way of working with file paths
 const path = require("path");
 
+const date = String(moment().format("ll")); //This assigns the constant date to the stringified version of the current date, obtained through moment.js, in the format MMM D, YYYY.
+
+const time = String(moment().format("LTS")); //This assigns the constant time to the stringified version of the current time, obtained through moment.js, in the format H:MM:SS PM/AM:
+
+const month = moment().format("MMM"); //This assigns the constant month to the stringified version of the current month, obtained through moment.js, in the format MMM.
+
+const year = moment().year();
+
 // log events is promising to take the parameters from the emitter
 // listener in routes and send that data to the try / accept block
 // fileLineItem is the information that is written to the log file
-const logEvents = async (msg, theDatabase) => {
-  const dateTime = `${format(new Date(), "MMM-dd-yyyy \tHH:mm:ss")}`;
-  const fileLineItem = `${dateTime} \t${msg} \t${theDatabase} \t${uuid()}`;
+const queryEvents = async (msg, theDatabase) => {
+  const fileLineItem = `${date} \t${time} \t${msg} \t${theDatabase} \t${uuid()}`;
 
   // try catch block is used to evaluate if 1 there is a filefolder to place the line item, if is does exit
   // it will take the fileLineItem and append it to the awaiting file.  If it does not exit then the folder is created
@@ -26,26 +34,27 @@ const logEvents = async (msg, theDatabase) => {
   // catch will handle any error that occurs if the try fails
 
   try {
-    if (theDatabase === "MongoDb") {
-      //   if (!fs.existsSync(path.join(__dirname, "logs"))) {
-      //     await fsPromises.mkdir(path.join(__dirname, "log"));
-      if (!fs.existsSync(`./${month} ${year}`)) {
-        await fsPromises.mkdir(`./${month} ${year}`);
+    if (theDatabase === "MongoDB") {
+      //CHANGE THIS DEPENDING ON WHAT WE CALL IT
+      if (!fs.existsSync(path.join(__dirname, `${month} ${year}_query_logs`))) {
+        await fsPromises.mkdir(
+          path.join(__dirname, `${month} ${year}_query_logs`)
+        );
       }
-      const fileName =
-        `${format(new Date(), "yyyyMMdd")}` + "_mongodb_queries.log";
+      const fileName = `${date}` + "_mongodb_queries.log";
       await fsPromises.appendFile(
-        path.join((__dirname, `${month} ${year}`, date)),
+        path.join(__dirname, `${month} ${year}_query_logs`, fileName),
         fileLineItem + "\n"
       );
     } else if (theDatabase === "Postgres") {
-      if (!fs.existsSync(path.join(`./${month} ${year}`))) {
-        await fsPromises.mkdir(path.join(`./${month} ${year}`));
+      if (!fs.existsSync(path.join(__dirname, `${month} ${year}_query_logs`))) {
+        await fsPromises.mkdir(
+          path.join(__dirname, `${month} ${year}_query_logs`)
+        );
       }
-      const fileName =
-        `${format(new Date(), "yyyyMMdd")}` + "_postgres_queries.log";
+      const fileName = `${date}` + "_postgres_queries.log";
       await fsPromises.appendFile(
-        path.join((__dirname, `${month} ${year}`, date)),
+        path.join(__dirname, `${month} ${year}_query_logs`, fileName),
         fileLineItem + "\n"
       );
     }
@@ -54,4 +63,4 @@ const logEvents = async (msg, theDatabase) => {
   }
 };
 
-module.exports = logEvents;
+module.exports = queryEvents;
