9488fea39b3c6a86d35f5921ddd3e69967675a38
kbalsom <kara.balsom@keyin.com>
2022-12-09 12:20:12 -0330

    Merging

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Date: Nov 25, 2022
 // Assignment: QAP3
 // Course Name: Full Stack JavaScript
-// Written By: David Turner
+// Written By: Kara Balsom
 
 // When a path in the NAV on the browser is selected it will route to the named JS file in the routes or routes api folder respectively.  That selection will trigger an HTTP method either upon selection or as a listener is pressed such as a button or <a> tag link, resulting call is built like a function.  The functions here are build around GET/POST/PUT/PATCH/DELETE requests which will then go to the DAL (data access layer) where the data from the specified collection in mongo is retrieved, that data is then parsed and sent back through functions GET/POST/PUT/PATCH/DELETE that were requested.  This data will be displayed through the approriate EJS file and subsequently in the browser.
 
