d5704064916f96e91db76dc496219e05bdbc8f90
David Turner <davidbrockturner@hotmail.com>
2022-12-20 16:18:54 -0330

    updated

--- services/pgdb.js
+++ services/pgdb.js
@@ -2,8 +2,8 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
-  database: "sprint2",
-  password: "Keyin2021",
+  database: "fullstack_sprint2",
+  password: "MacAlex",
   port: 5432,
 });
 module.exports = pool;
