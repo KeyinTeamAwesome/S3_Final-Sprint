e91e2c7a8a15df03868e1e38c186c0d3a7fa0e8b
kbalsom <kara.balsom@keyin.com>
2022-12-20 15:32:03 -0330

    Search test added

---
+++ __test__/search.test.js
@@ -0,0 +1,35 @@
+const { getMovies } = require("../services/search.dal");
+
+require("dotenv").config();
+const dal = require("../services/mdb");
+
+describe("Testing getMovies function", () => {
+  // Setting up db connection
+  beforeAll(async () => {
+    try {
+      await dal.connect();
+      global.userCollection = dal.db("sprint2").collection("movies");
+      global.DEBUG = false;
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  afterAll(async () => {
+    // Close Database here
+    dal.close();
+  });
+
+  test("addLogin function testing", async () => {
+    const searchTerm = "Due Date";
+    const database = "mongodb";
+    let query = await getMovies(searchTerm, database);
+    expect(query).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          title: "Due Date",
+        }),
+      ])
+    );
+  });
+});
