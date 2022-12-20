7ad2cfca62388b585d20a400cad68229308658d2
kbalsom <kara.balsom@keyin.com>
2022-12-20 16:35:43 -0330

    DB commenting

--- services/mdb.js
+++ services/mdb.js
@@ -1,8 +1,8 @@
-const { MongoClient } = require("mongodb");
-require("dotenv").config();
+const { MongoClient } = require("mongodb"); //Require mongodb.
+require("dotenv").config(); //Configure dotenv
 
 // .env file must contain: ATLAS_URI=<your mongo connection string>
-const uri = process.env.ATLAS_URI;
-const pool = new MongoClient(uri);
+const uri = process.env.ATLAS_URI; //Assign constant uri to the ATLAS_URI.
+const pool = new MongoClient(uri); //Assign constant pool the new MongoClient uri.
 
-module.exports = pool;
+module.exports = pool; //Export pool.
--- services/pgdb.js
+++ services/pgdb.js
@@ -1,9 +1,10 @@
-const Pool = require("pg").Pool;
+const Pool = require("pg").Pool; //Require Pool
 const pool = new Pool({
+  //Set up a new Pool with your postgres user/database information, and assign it to the constant pool.
   user: "postgres",
   host: "localhost",
   database: "fullstack_sprint2",
   password: "MacAlex",
   port: 5432,
 });
-module.exports = pool;
+module.exports = pool; //Export pool.
