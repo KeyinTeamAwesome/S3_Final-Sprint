aef98b8c7ebd3e74e9049667c6b19adac626695b
kbalsom <kara.balsom@keyin.com>
2022-12-20 17:38:42 -0330

    auth commenting

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const bcrypt = require("bcrypt");
-const uuid = require("uuid");
-const router = express.Router();
-const { addLogin, getLoginByUsername } = require("../services/auth.dal");
+const express = require("express"); //Require express and assign it to the constant express.
+const bcrypt = require("bcrypt"); //Require bycrypt and assign it to the constant bcrypt.
+const uuid = require("uuid"); //Require uuid and assign it to the constant uuid.
+const router = express.Router(); //Assign router to the app constant.
+const { addLogin, getLoginByUsername } = require("../services/auth.dal"); //Import Functions from auth.dal
 const fs = require("fs");
 // This will bring in the "events" global object no npm install required
 const eventEmmitter = require("events");
@@ -21,44 +21,45 @@ myEmitter.on("status", (msg, theStatusCode) => {
   logEvents(msg, theStatusCode);
 });
 
-router.use(express.static("public")); // May not be necessary since it's in index.js
+router.use(express.static("public"));
 
 router.get("/", async (req, res) => {
   if (DEBUG) console.log("login page: ");
-  res.render("search.ejs", { status: req.app.locals.status });
+  res.render("search.ejs", { status: req.app.locals.status }); //Render search.ejs
 });
 
 router.post("/", async (req, res) => {
   try {
     if (DEBUG) console.log("auth.getLoginByUsername().try");
-    let user = await getLoginByUsername(req.body.username);
+    let user = await getLoginByUsername(req.body.username); //Wait for the getLoginByUsername function, which is passed the username. Assign returned results to user.
     if (DEBUG) console.log(user);
     if (user === undefined) {
+      //If user is undefined, send status below to user.
       req.app.locals.status = "Incorrect user name was entered.";
-      res.redirect("/auth");
+      res.redirect("/auth"); //Redirect to /auth.
     }
     if (await bcrypt.compare(req.body.password, user.password)) {
       // change using app.locals to use session or java web token (jwt)
       req.app.locals.user = user;
       req.app.locals.status = "Happy for your return " + user.username;
-      res.redirect("/search/");
+      res.redirect("/search/"); //Redirect to search.
     } else {
       req.app.locals.status = "Incorrect password was entered.";
-      res.redirect("/auth");
+      res.redirect("/auth"); //Redirect to /auth.
     }
   } catch (error) {
-    console.log(error);
+    console.log(error); //Log the error.
     if (DEBUG) console.log("auth.getLoginByUsername().catch: " + user.username);
     theStatusCode = 503;
     msg = `Error`;
-    myEmitter.emit("status", msg, theStatusCode);
+    myEmitter.emit("status", msg, theStatusCode); //logEvents emitter.
     res.render("503.ejs");
   }
 });
 
 // from http browser it has /auth/new
 router.get("/new", async (req, res) => {
-  res.render("register.ejs", { status: req.app.locals.status });
+  res.render("register.ejs", { status: req.app.locals.status }); //Render register.ejs.
 });
 
 router.post("/new", async (req, res) => {
@@ -91,7 +92,7 @@ router.post("/new", async (req, res) => {
     console.log(error);
     theStatusCode = 503;
     msg = `Error`;
-    myEmitter.emit("status", msg, theStatusCode);
+    myEmitter.emit("status", msg, theStatusCode); //logEvents emitter.
     res.render("503.ejs");
     // log this error to an error log file.
   }
@@ -99,7 +100,7 @@ router.post("/new", async (req, res) => {
 
 router.get("/exit", async (req, res) => {
   if (DEBUG) console.log("get /exit");
-  res.redirect("/");
+  res.redirect("/"); //Redirect to /.
 });
 
-module.exports = router;
+module.exports = router; //Export router.
